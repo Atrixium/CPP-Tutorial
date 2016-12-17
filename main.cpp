4244b88c81ff61363705dbc9307dd312898f6051
Atrixium <rob.ss.bass@gmail.com>
2016-12-16 23:59:33 -0800

    Init SDL, create window, create renderer, load bmp, display on screen. Added .gitignore to omit obj and bin folders

--- main.cpp
+++ main.cpp
@@ -1,1 +1,63 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <string>
 
+int main(int argc, char **argv)
+{
+    if (SDL_Init(SDL_INIT_VIDEO) !=0)
+    {
+        std::cout << "SDL Init error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("Hello World", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (window == nullptr)
+    {
+        std::cout << "SDL_CreateWindow error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr)
+    {
+        SDL_DestroyWindow(window);
+        std::cout << "SDL_CreateRenderer error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    std::string imagePath = "Images/Atrixium.bmp";
+    SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
+    if (bmp == nullptr)
+    {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        std::cout << "SDL_LoadBMP error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, bmp);
+    SDL_FreeSurface(bmp);
+    if (texture == nullptr)
+    {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        std::cout << "SDL_CreateTextureFromSurface error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(3000);
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+
+    return 0;
+}
